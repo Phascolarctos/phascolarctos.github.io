3142e30a4407b5b98545c6d6e9080d3e2f1c19d7
Phascolarctos <sixbugsafe@gmail.com>
2024-11-12 13:00:44 +0800

    update article

---
+++ main.js
@@ -0,0 +1,39 @@
+const kvs = [
+    ["df", {
+        tlt: "default article title",
+        ctt: "article content list something about the source of website, again choose me for you, hahaha..."
+    }],
+    ["rust", {
+        tlt: "rust learning",
+        ctt: "rust is a language"
+    }],
+    ["java", {
+        tlt: "java learning",
+        ctt: "java is a language"
+    }],
+];
+const m = new Map(kvs);
+const tmc = document.getElementById("tmc")
+setInterval(function () {
+    const date = new Date()
+    tmc.innerText = date.toUTCString()
+}, 0)
+
+const t = document.getElementById("tlt")
+const c = document.getElementById("ctt")
+
+const df = m.get("df")
+t.innerText = df.tlt
+c.innerText = df.ctt
+
+const dms = document.getElementsByClassName("att")
+for (let index = 0; index < dms.length; index++) {
+    const e = dms[index];
+    const k = e.textContent.trim().toLowerCase()
+    e.id = k
+    const v = m.get(k)
+    e.addEventListener("click", (event) => {
+        t.innerText = v.tlt
+        c.innerText = v.ctt
+    })
+}
